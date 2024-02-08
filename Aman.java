7ff85933158965289addf6755215bd54d3985087
Amanms1402 <119517551+Amanms1402@users.noreply.github.com>
2024-02-08 18:47:26 +0530

    first commit

---
+++ Aman.java
@@ -0,0 +1,5 @@
+public class Aman{
+    public static void main(String[] args){
+        System.out.println("I want to become an AIML engineer or developer");
+    }
+}
--- raj.java
+++
@@ -1,5 +0,0 @@
-public class raj{
-    public static void main(String[] args){
-        System.out.println("I want to become Java full stack developer "+"21BCE10414 "+"Tirthan Amit Prajapati"+"Advanced Java Programming");
-    }
-}
